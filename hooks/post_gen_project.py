3d95a84ede322651140c6c1fe2a8be02d1d351c7
Piotr Grędowski <piotrgredowski@gmail.com>
2023-09-21 17:17:27 +0200

    Initial commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import subprocess
+
+THIS_REPO_NAME = "python-template"
+THIS_REPO_URL = "https://github.com/piotrgredowski/python-template"
+
+
+def create_repo():
+    p = subprocess.Popen("git init -b main".split(" "))
+    p.wait()
+
+
+def get_commit_id_of_template_repo():
+    old_path = os.getcwd()
+    os.chdir(os.getenv("PWD", ""))
+    commit_id = subprocess.check_output("git rev-parse HEAD", shell=True).decode("utf-8").strip()
+    os.chdir(old_path)
+    return commit_id
+
+
+def get_commit_message():
+    commit_message = "Initial commit made by `THIS_REPO_NAME`"
+    commit_id = get_commit_id_of_template_repo()
+
+    if commit_id:
+        commit_message += f"""
+
+Automatically created from: [{THIS_REPO_NAME}]({THIS_REPO_URL}).
+Source commit: {commit_id[:10]}
+"""
+    return commit_message
+
+
+def commit_everything():
+    p = subprocess.Popen(["git", "add", "."])
+    p.wait()
+
+    p = subprocess.Popen(["git", "commit", "-m", get_commit_message()])
+    p.wait()
+
+
+create_repo()
+commit_everything()
---
+++ {{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,20 @@
+# {{cookiecutter.project_name}}
+
+{{cookiecutter.project_short_description}}
+
+## Pre-requisites
+
+- [Rye](https://github.com/mitsuhiko/rye)
+
+## Installation
+
+```bash
+rye sync
+```
+
+## Usage
+
+```bash
+rye run src.{{ cookiecutter.package_name }}
+
+```
